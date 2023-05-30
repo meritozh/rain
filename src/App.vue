7c474c7bb7af7b2bc74ebf5a4c10ad0049257afb
meritozh <ah841814092@gmail.com>
2023-05-30 22:59:54 +0800

    chore: make every ready

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: '沁苑',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'description', content: 'meritozh\'s showcase' },
     {
       name: 'theme-color',
       content: () => isDark.value ? '#00aba9' : '#ffffff',
